9ec1e8422695779dd69b79f0bb1b0cc425c2749e
Mohamad Balawi <malek22139@gmail.com>
2023-05-06 21:08:57 +0300

    Undoing static  change

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,24 @@
 import cx from 'classnames';
 import React, { useState } from 'react';
 import SVG from 'react-inlinesvg';
+import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, useNavigate } from 'react-router-dom';
+import { authActions } from './../../slices/auth.slice';
 import CartIcon from './CartIcon';
 import HamburgerMenu from './HamburgerMenu';
 import './Navbar.scss';
 import NavIcon from './NavIcon';
 
 export default function Navbar() {
-	// const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+	const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 	const [clicked, setClicked] = useState(false);
-	// const dispatch = useDispatch();
+	const dispatch = useDispatch();
 	const navigate = useNavigate();
 
-	// function logoutHandler() {
-	// 	dispatch(authActions.logout());
-	// 	navigate('/login');
-	// }
+	function logoutHandler() {
+		dispatch(authActions.logout());
+		navigate('/login');
+	}
 	const logoFilename = 'logo.svg';
 	return (
 		<nav>
@@ -35,13 +37,11 @@ export default function Navbar() {
 				</NavLink>
 				<NavLink to='/orders'>Orders</NavLink>
 				{/* <NavLink to='/signup'>Signup</NavLink> */}
-				{/* <NavLink to='/dashboard'>Dashboard</NavLink> */}
-				{/* {isLoggedIn ? (
-					<button onClick={logoutHandler}>Logout</button>
+				{isLoggedIn ? (
+					<a onClick={logoutHandler}>Logout</a>
 				) : (
 					<NavLink to='/login'>Login</NavLink>
-				)} */}
-				{/* <NavLink to='/pizza'>Pizza</NavLink> */}
+				)}
 			</div>
 			<NavIcon></NavIcon>
 
